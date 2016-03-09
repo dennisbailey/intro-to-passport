108aa8de21bbe51c28d91b17e47b117321119bb2
Dennis Bailey <bailey.dennis@gmail.com>
2016-03-09 10:27:17 -0700

    Conitnuing to setup passport for localstrategy

---
+++ src/server/lib/auth.js
@@ -0,0 +1,15 @@
+var passport = require('pasport');
+var LocalStrategy = require('passport-local').Strategy;
+var knex = require('knex');
+
+passport.use(new LocalStrategy(
+  function(email, password, done) {
+    
+//     User.findOne({ email: email }, function (err, email) {
+//       if (err) { return done(err); }
+//       if (!email) { return done(null, false); }
+//       if (!email.verifyPassword(password)) { return done(null, false); }
+//       return done(null, user);
+//     });
+  }
+));
