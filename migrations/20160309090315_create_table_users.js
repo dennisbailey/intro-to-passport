9f73e8ca04bfc0647034bce980e643235116ca77
Dennis Bailey <bailey.dennis@gmail.com>
2016-03-09 09:26:24 -0700

    Added not nullable and unique constraints to the users table

--- migrations/20160309090315_create_table_users.js
+++ migrations/20160309090315_create_table_users.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function(table){
     table.increments();
-    table.string('email');
-    table.string('password');   
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();   
   })  
 };
 
