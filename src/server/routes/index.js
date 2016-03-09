d2939d53dedf21b03358fffa8feb83a9c46a74a2
Dennis Bailey <bailey.dennis@gmail.com>
2016-03-09 09:41:50 -0700

    Added a login page with a form and an applicable route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Introduction to Passport' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
