f0591cec6ab2f18c9d6755bb639617380f155d7f
Dennis Bailey <bailey.dennis@gmail.com>
2016-03-09 09:46:54 -0700

    Added a post route to submit a login

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,4 +9,17 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
+router.post('/login', function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password
+  
+  res.render('login', { 
+    title: 'Login',
+    email : email,
+    password : password 
+    }
+  );
+  
+});
+
 module.exports = router;
